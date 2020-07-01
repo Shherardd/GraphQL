918e6231ba33a062cf9abb10249503ddc4b53b41
Luis Gerardo <gerardwjones@gmail.com>
2020-06-30 23:50:34 -0500

    GraphiQl middelware added to Express server

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 'use strict'
 
 const { graphql, buildSchema} = require ('graphql')
+const express = require ('express')
+const gqlMiddleware = require('express-graphql')
+
+const app = express()
+const port = process.env.PORT || 3000
+
 
 // define schema
 const schema = buildSchema(`
     type Query {
-        hello: String
+        hello: String,
+        saludo: String
     }
 `)
 
-graphql(schema, '{ hello }').then((data) => {
+// Config resolvers
+
+const resolvers = { 
+    hello: () => {return 'hello world'},
+    saludo: () => {return 'Hola a todos'}
+}
+
+app.use('/api', gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+}))
+
+app.listen(port, function(){
+    console.log(`server listening at: localhost:${port}`)
+})
+
+
+graphql(schema, '{ saludo hello }', resolvers).then((data) => {
     console.log(data)
 })
