055bb28379693421b52ecf13f35b0ba7d63f8148
Luis Gerardo <gerardwjones@gmail.com>
2020-07-01 02:08:47 -0500

    2:07 update 7/1/2020

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
 'use strict'
 
-const { graphql, buildSchema} = require ('graphql')
-const express = require ('express')
+const { buildSchema } = require('graphql')
+const express = require('express')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-
 // define schema
-const schema = buildSchema(`
-    type Query {
-        hello: String,
-        saludo: String
-    }
-`)
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
-// Config resolvers
-
-const resolvers = { 
-    hello: () => {return 'hello world'},
-    saludo: () => {return 'Hola a todos'}
-}
 
 app.use('/api', gqlMiddleware({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: true
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
-app.listen(port, function(){
-    console.log(`server listening at: localhost:${port}`)
-})
-
-
-graphql(schema, '{ saludo hello }', resolvers).then((data) => {
-    console.log(data)
+app.listen(port, function () {
+  console.log(`server listening at: localhost:${port}`)
 })
